182a0b4bfecd65b1ac8e8d7ef150bc3e029a7569
092vk <vk092kumar@gmail.com>
2024-02-01 23:44:17 +0530

    added files

---
+++ Day10memoryStackHeap.js
@@ -0,0 +1,41 @@
+//stack (primitive) ---> gives you copy of memory 
+//queue (Non - primitive ) ---> gives you reference of memory
+
+//****  stack  ***** */
+let myname = "vivek kumar";
+console.log(myname);
+let mylastname = myname;
+mylastname = "yadav";
+console.log(myname);
+console.log(mylastname);
+
+//this is happening becuz myname is a string(primitive data type ) , resulting in mylastname getting a copy of myname , behind the scenes we are using stack and each time we assign primitive data type we are actually storing a copy of original , changing which does not effect the original 
+
+
+/********Heap******/
+let userOne = {
+    email : "vk092@yahoo.com",
+    naam : "vivek kumar"
+};
+
+let userTwo = {
+    email : "shivam083@gmail.com",
+    naam : "shivam"
+};
+console.log(userOne);
+console.log(userTwo);
+
+userTwo = userOne;
+userTwo.email= "sunny@youngBlood.com";
+console.log(userTwo);
+console.log(userOne);
+
+
+/*******as we can see , just becuz we are changing userTwo , userOne is also getting changed , this is becuz they are non-primitive data structure and such types are saved in heaps , heaps store data , and the variables stores the refrence to this data , so userOne and usertwo both are refrencing the same memory , thus changing userTwo , automatically changes userOne ******/
+
+
+
+
+
+
+
---
+++ Day4Var.js
@@ -0,0 +1,21 @@
+const naam = "vivek kumar";
+var roll = "22bec092";
+let className = "ece";//remeber the variable name "class" is a reserved keyword in js , you can't use it 
+city = "patna";
+
+console.table([naam,roll,className]);
+
+// naam = "shivam"; ** can't change it as it is a const value 
+console.log(naam);
+
+
+//changing values 
+roll = "11";
+className = "cse";
+city = "jaipur";
+
+console.table([roll,className,city]);
+ 
+
+
+
