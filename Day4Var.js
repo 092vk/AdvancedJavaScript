197ae0cd8253472eebb760d6520e94c34b141f17
092vk <vk092kumar@gmail.com>
2024-02-02 07:50:44 +0530

    l

--- Day4Var.js
+++ Day4Var.js
@@ -16,6 +16,17 @@ city = "jaipur";
 
 console.table([roll,className,city]);
  
+//as we can see , we can change values of var,let keyword declared variables , var is old method which don't care about the block , so it's use should be avoided , and let should be used more often together with const 
+
+//what about declaring without any keyword - if you declare variable without any keyword 
+//1.if it is declared without any scope then it is global 
+//2.but if it is declared inside a scope it will remain inside that block 
+
+
+let crushNaam;
+console.log(crushNaam);
+//this gives undefined 
 
 
 
+//only use let and const in js 
