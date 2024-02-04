ddea31690aa8b18031da75f2f7e9da872a680cf5
092vk <vk092kumar@gmail.com>
2024-02-04 12:08:24 +0530

    day7

---
+++ basic1/Day7CoersionString.js
@@ -0,0 +1,45 @@
+// ************** operations on string *************
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+// mathematical operations 
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+//string addition 
+let firstName = "vivek";
+let lastName = " kumar";
+console.log(firstName+lastName);
+
+
+// weird js string and number addition 
+console.log("2"+2);//22
+console.log(2+"2");//22
+console.log(2+2+"2");//42
+console.log("2"+2+2);//222
+//dont depend upon them , write clear and proper code and dont depend on js for auto type conversion
+
+
+//other Misc conversions
+console.log(+true);//converts true --> 1
+console.log(+"");//converts "" -->0
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2;
+console.log(num1 , num2, num3);
+//not prefered
+
+
+//prefix and sufix operators 
+let gameCounter = 100;
+console.log(++gameCounter);//first increment and then use
+console.log(gameCounter++);//first assignment and then increment 
+console.log(gameCounter);//increment result 
+
+
+
+
