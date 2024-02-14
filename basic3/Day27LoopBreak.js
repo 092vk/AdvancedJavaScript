fd947dc652913fa9f39785629b6c51bdc1e69d3d
092vk <vk092kumar@gmail.com>
2024-02-14 18:42:38 +0530

    a

---
+++ basic3/Day27LoopBreak.js
@@ -0,0 +1,17 @@
+// for loop
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
+
+
+// nested loops 
+for (let i = 0; i <10; i++) {
+    console.log(`outer value : ${i}`);
+    for(let j=0;j<3;j++){
+        console.log(`inner value ${j} and inner :${i}`);
+    }
+    
+}
+
+
---
+++ basic3/Day28WhileDo.js
@@ -0,0 +1,6 @@
+console.log("belkclwn");
+
+
+let date = new Date();
+console.log("hello");
+console.log(date.toLocaleTimeString());
