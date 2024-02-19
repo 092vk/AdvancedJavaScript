2d86016f97aba5cd79ae980c1271a0d9ed3afb31
092vk <vk092kumar@gmail.com>
2024-02-19 22:17:20 +0530

    add

--- basic4/Day32HtmlColl&Nodelist.js
+++ basic4/Day32HtmlColl&Nodelist.js
@@ -1,2 +1,23 @@
 // dom selectors , html collections and mode lists 
 
+//document.getElementById('id_value');
+//document.getElementsByClassName("value");
+//document.getElementsByTagName("value");
+//document.getElementByName("value")
+
+
+//document.querySelector('h2');
+//document.querySelectorAll('h2');
+//document.querySelector('.className');
+//document.querySelector("#idName");
+//document.querySelector("input[type="password"]);
+
+
+// document.getElementById('value').getAttribute('gg')
+// document.getElementById('value').id
+// setAttribute('class','test oldOne');
+
+
+
+// NodeList--***
+
