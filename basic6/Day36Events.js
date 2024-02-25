ef44662501d326da7383d263c80089a61b45ace6
092vk <vk092kumar@gmail.com>
2024-02-25 11:03:50 +0530

    event

--- basic6/Day36Events.js
+++ basic6/Day36Events.js
@@ -44,9 +44,62 @@ document.getElementById('flr1').addEventListener('click',(eventObject)=>{
 
 
 
+
+
+
+
+
+
+
+
 // event propogation(boolean parameter of addEventListner)
 // 1. event bubling - false , event capturing -true
 
 
+
 // lets say we have images inside a ul  , and we have two event listner , one on img and other on ul , now  if we click on img which is inside ul , which event will run first and how the event will propagate .
 
+
+// 1.event bubbling -- > false(default value)
+document.getElementsByClassName('list')[0].addEventListener('click',(eventObject)=>{
+    alert("you have clicked on the list");
+    console.log("you have entered the list");
+    eventObject.stopPropagation();//this will stop the propagation of the event further down -- in case of true or event capturing 
+})//true
+
+document.getElementById('flr3').addEventListener('click',()=>{
+    alert("you have clicked the flr3");
+    console.log("flr3 was clicked");
+})
+
+
+// when we dont give any argument to event listner , it takes false by default parameter for event propagation --> this is called as event bubbling , first the inner event will run and then it bubble up .
+
+
+// 2.event capturing
+document.getElementById('flr4').addEventListener('click',(eventObject)=>{
+    console.log("you have clicked on flr4");
+    alert("you have clicked on flr4");
+    
+},true);
+// for this to work both inner and outer eventListner must be true , --> in this case the outer eventListener works first and then the inner one 
+
+
+
+
+// we can use ****eventObject.stopPropagation()****** to stop the propogation of the event in lower or higher tags 
+
+
+
+document.getElementById('gogly').addEventListener('click',function(eventObject){
+    console.log("clicked");
+    eventObject.preventDefault();
+    console.log("google didn't open as we prevented the default to happen ");
+},false)
+// we can use preventDefault() to stop the default from happening 
+
+
+
+
+
+// Q. when clicked on the image , dissapear it 
